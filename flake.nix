6b9df831c4c1fd8978dadc461998713ae8923261
siggsy <git@siggsy.xyz>
2025-05-21 10:31:16 +0200

    Use global nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs";
   };
 
   outputs =
